0df561698cd9b3eaa79eb17e442a920b2793b8d5
nicolassousa <nicolas.p.sousa@outlook.com>
2021-04-27 18:55:02 +0100

    version 2.1

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const controllerhubspot = require('../controllers/hubspot.controller.js');
 const controllerJasmin = require('../controllers/jasmin.controller');
 const controllerauxJasmin = require('../controllers/jasmin.aux.controller');
-const adyen = require('../controllers/adyen.encrypt');
 
 //HUBSPOT
 router.post('/Hubspot/', controllerhubspot.createClient);
