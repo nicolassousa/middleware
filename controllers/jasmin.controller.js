75694f060a50e1291a0ba613403de2866b832cc0
nicolassousa <nicolas.p.sousa@outlook.com>
2021-04-28 18:46:26 +0100

    Correcao Bug

--- controllers/jasmin.controller.js
+++ controllers/jasmin.controller.js
@@ -43,101 +43,110 @@ function registarCompra(request, response) {
                             if (res.body.length == 0) {
 
                                 jasminAux.insertClient(email, access_token, (res) => {
-                                    if (res.statusCode == 200) {
-                                        const user = res.body.customer_id;
-
-                                        if(productSold.length != 0){
-                                            let json = {
-                                                'documentType': 'FR',  //Tipo de Documento - Fatura Recibo
-                                                'serie': invoiceType.serie, //Serie a que a fatura pertence - ex: 2021 (faturas do ano 2021)
-                                                'seriesNumber': invoiceType.currentNumber, //Chave da série
-                                                'company': 'RUM', //Empresa 
-                                                'paymentTerm': '00', //Pronto Pagamento
-                                                'paymentMethod': 'MB', //Método Pagamento MB
-                                                'currency': 'EUR', //Moeda
-                                                'documentDate': new Date().toISOString(), //Data em que o documento foi instanciado
-                                                'postingDate': new Date().toISOString(), // Data em que o documento foi criado
-                                                'buyerCustomerParty': user.partyKey, //ID do cliente que realiza a compra
-                                                'buyerCustomerPartyName': user.name, //Nome do Cliente
-                                                'buyerCustomerPartyTaxId': user.companyTaxID, //NIF do cliente
-                                                'exchangeRate': 1,
-                                                'discount': 0, //Percentagem de desconto
-                                                'loadingCountry': 'PT', //País de venda
-                                                'unloadingCountry': 'PT', //País de Compra
-                                                'financialAccount': '01', //Conta 01 (Empresa RUM)
-                                                'isExternal': false,
-                                                'isManual': false,
-                                                'isSimpleInvoice': false,
-                                                'isWsCommunicable': false,
-                                                'deliveryTerm': 'NA', //Termo de entrega - Nao Aplicavel
-                                                'documentLines': productSold,
-                                                'WTaxTotal': { //Pagamento de Taxas
-                                                    'amount': 0,
-                                                    'baseAmount': 0,
-                                                    'reportingAmount': 0,
-                                                    'fractionDigits': 2,
-                                                    'symbol': '€'
-                                                },
-                                                'TotalLiability': { //Preco Liquido
-                                                    'baseAmount': 0,
-                                                    'reportingAmount': 0,
-                                                    'fractionDigits': 2,
-                                                    'symbol': '€'
-                                                }
-                                            }
-
-                                            let options = {
-                                                headers: {
-                                                    'Authorization': `Bearer ${access_token}`,
-                                                    'Content-Type': 'application/json; charset=utf-8',
-                                                    'Content-Length': JSON.stringify(json).length
-                                                },
-                                                url: `${url_jasmin}billing/invoices`,
-                                                body: JSON.stringify(json)
-                                            }
-                    
-                                            req.post(options, (err, res) => {
-                                                if (!err && res.statusCode == 201) {
-                                                    let numeroSenhas;
-                                                    if(IdProduto == 'PACKSENHAS'){
-                                                        numeroSenhas = 10;
-                                                    } else{
-                                                        numeroSenhas = 1;
-                                                    }
-
-                                                    jasminAux.acertoStock(IdProduto, access_token, (res) => {
-                                                        if(res.statusCode == 201){
-                                                            hubspot.updateClient(email, numeroSenhas, (res) => {
-                                                                if(res.statusCode == 200){
-                                                                    jasminAux.getPDFDocument(access_token, idFatura,(res) => {
-                                                                        if(res.statusCode == 200){
-                                                                            const pdf = res.body;
-                                                                            jasminAux.sendPDF(email, pdf);
-                                                                            return response.status(200).json({
-                                                                                message: 'success'
-                                                                            });
-                                                                        }
-                                                                        else{
-                                                                            return response.status(400).json({
-                                                                                message: res.body
-                                                                            });
-                                                                        }
-                                                                    })
-                                                                }
-                                                            })
-                                                        }else{
-                                                            return response.status(400).json({
-                                                                message: res.body
-                                                            });
-                                                        }
-                                                    })
-                                                } else {
+                                    if (res.statusCode == 200) {                                        
+                                        jasminAux.checkUser(email, access_token, (res1) => {
+                                            if (res1.statusCode == 200) {
+                                                if (res1.body.length == 0) {
                                                     return response.status(400).json({
-                                                        message: res.body
+                                                        message: res1.body
                                                     });
+                                                }else{
+                                                    const user1 = res.body[0];
+                                                    if(productSold.length != 0){
+                                                        let json = {
+                                                            'documentType': 'FR',  //Tipo de Documento - Fatura Recibo
+                                                            'serie': invoiceType.serie, //Serie a que a fatura pertence - ex: 2021 (faturas do ano 2021)
+                                                            'seriesNumber': invoiceType.currentNumber, //Chave da série
+                                                            'company': 'RUM', //Empresa 
+                                                            'paymentTerm': '00', //Pronto Pagamento
+                                                            'paymentMethod': 'MB', //Método Pagamento MB
+                                                            'currency': 'EUR', //Moeda
+                                                            'documentDate': new Date().toISOString(), //Data em que o documento foi instanciado
+                                                            'postingDate': new Date().toISOString(), // Data em que o documento foi criado
+                                                            'buyerCustomerParty': user1.partyKey, //ID do cliente que realiza a compra
+                                                            'buyerCustomerPartyName': user1.name, //Nome do Cliente
+                                                            'buyerCustomerPartyTaxId': user1.companyTaxID, //NIF do cliente
+                                                            'exchangeRate': 1,
+                                                            'discount': 0, //Percentagem de desconto
+                                                            'loadingCountry': 'PT', //País de venda
+                                                            'unloadingCountry': 'PT', //País de Compra
+                                                            'financialAccount': '01', //Conta 01 (Empresa RUM)
+                                                            'isExternal': false,
+                                                            'isManual': false,
+                                                            'isSimpleInvoice': false,
+                                                            'isWsCommunicable': false,
+                                                            'deliveryTerm': 'NA', //Termo de entrega - Nao Aplicavel
+                                                            'documentLines': productSold,
+                                                            'WTaxTotal': { //Pagamento de Taxas
+                                                                'amount': 0,
+                                                                'baseAmount': 0,
+                                                                'reportingAmount': 0,
+                                                                'fractionDigits': 2,
+                                                                'symbol': '€'
+                                                            },
+                                                            'TotalLiability': { //Preco Liquido
+                                                                'baseAmount': 0,
+                                                                'reportingAmount': 0,
+                                                                'fractionDigits': 2,
+                                                                'symbol': '€'
+                                                            }
+                                                        }
+            
+                                                        let options = {
+                                                            headers: {
+                                                                'Authorization': `Bearer ${access_token}`,
+                                                                'Content-Type': 'application/json; charset=utf-8',
+                                                                'Content-Length': JSON.stringify(json).length
+                                                            },
+                                                            url: `${url_jasmin}billing/invoices`,
+                                                            body: JSON.stringify(json)
+                                                        }
+                                
+                                                        req.post(options, (err, res) => {
+                                                            if (!err && res.statusCode == 201) {
+                                                                let numeroSenhas;
+                                                                if(IdProduto == 'PACKSENHAS'){
+                                                                    numeroSenhas = 10;
+                                                                } else{
+                                                                    numeroSenhas = 1;
+                                                                }
+            
+                                                                jasminAux.acertoStock(IdProduto, access_token, (res) => {
+                                                                    if(res.statusCode == 201){
+                                                                        hubspot.updateClient(email, numeroSenhas, (res) => {
+                                                                            if(res.statusCode == 200){
+                                                                                jasminAux.getPDFDocument(access_token, idFatura,(res) => {
+                                                                                    if(res.statusCode == 200){
+                                                                                        const pdf = res.body;
+                                                                                        jasminAux.sendPDF(email, pdf);
+                                                                                        return response.status(200).json({
+                                                                                            message: 'success'
+                                                                                        });
+                                                                                    }
+                                                                                    else{
+                                                                                        return response.status(400).json({
+                                                                                            message: res.body
+                                                                                        });
+                                                                                    }
+                                                                                })
+                                                                            }
+                                                                        })
+                                                                    }else{
+                                                                        return response.status(400).json({
+                                                                            message: res.body
+                                                                        });
+                                                                    }
+                                                                })
+                                                            } else {
+                                                                return response.status(400).json({
+                                                                    message: res.body
+                                                                });
+                                                            }
+                                                        })
+                                                    }
                                                 }
-                                            })
-                                        }
+                                            }
+                                        })
                                     }
                                     else {
                                         return response.status(400).json({
